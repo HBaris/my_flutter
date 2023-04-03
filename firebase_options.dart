acd92426eca104f797860c417fca87eb3cf6dd71
HBado <nes77850@gmail.com>
2023-04-03 23:53:57 +0300

    sildum

--- firebase_options.dart
+++ firebase_options.dart
@@ -1,8 +1,1 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
-// ...
-
-await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-);
